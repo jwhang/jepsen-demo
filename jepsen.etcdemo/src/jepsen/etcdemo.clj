e700cb771d030a5e4b947043f33c07622caf22b5
Joel Whang <joelwhang@berkeley.edu>
2018-11-28 21:57:04 +0000

    serve jepsen web server

--- jepsen.etcdemo/src/jepsen/etcdemo.clj
+++ jepsen.etcdemo/src/jepsen/etcdemo.clj
@@ -1,6 +1,18 @@
-(ns jepsen.etcdemo)
+(ns jepsen.etcdemo
+  (:require [jepsen.cli :as cli]
+            [jepsen.tests :as tests]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+
+(defn etcd-test
+  "Take cli options and construct test map"
+  [opts]
+  (merge tests/noop-test
+         opts))
+
+(defn -main
+  "Runs command line args!"
+  ; Comments with semicolons
+  [& args]
+  (cli/run! (merge (cli/single-test-cmd {:test-fn etcd-test})
+                   (cli/serve-cmd))
+            args))
