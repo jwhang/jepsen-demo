5474469d46343997b5b34727200e5fb96322b58b
Joel Whang <joelwhang@berkeley.edu>
2018-11-28 23:19:38 +0000

    update setup for leader election

--- jepsen.etcdemo/src/jepsen/etcdemo.clj
+++ jepsen.etcdemo/src/jepsen/etcdemo.clj
@@ -9,6 +9,28 @@
             [jepsen.os.debian :as debian]))
 
 (def dir "/opt/etcd")
+(def binary "etcd")
+(def pidfile (str dir "/etcd.pid"))
+(def logfile (str dir "/etcd.log"))
+
+(defn node-url
+  [node port]
+  (str "http://" node ":" port))
+
+(defn client-url
+  [node]
+  (node-url node 2379))
+
+(defn peer-url
+  [node]
+  (node-url node 2380))
+
+(defn initial-cluster
+  [test]
+  (->> (:nodes test)
+    (map (fn [node]
+         (str node "=" (peer-url node))))
+    (str/join ",")))
 
 (defn db
   "Constructs a database for given etcd version"
@@ -19,9 +41,29 @@
       (c/su
         (let [url (str "https://storage.googleapis.com/etcd/" version
                        "/etcd-" version "-linux-amd64.tar.gz")]
-          (cu/install-archive! url dir))))
+          (cu/install-archive! url dir)
+          (cu/start-daemon!
+            {:logfile logfile
+             :pidfile pidfile
+             :chdir   dir}
+            binary
+            :--log-output         :stderr
+            :--name               node
+            :--listen-peer-urls   (peer-url node)
+            :--listen-client-urls (client-url node)
+            :--advertise-client-urls       (client-url node)
+            :--initial-cluster-state       :new
+            :--initial-advertise-peer-urls (peer-url node)
+            :--initial-cluster             (initial-cluster test))
+          (Thread/sleep 10000))))
     (teardown! [_ test node]
-      (info "tearing down etcd"))))
+      (info "tearing down etcd")
+      (cu/stop-daemon! binary pidfile)
+      (c/su (c/exec :rm :-rf dir)))
+
+    db/LogFiles
+    (log-files [_ test node]
+      [logfile])))
 
 (defn etcd-test
   "Take cli options and construct test map"
